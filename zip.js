9fcaf666591731b3d1b121f5192828d85103f933
Nabeel Valley <nabeelvalley@outlook.com>
2021-12-20 21:42:27 +0200

    :wrench: change resolution for images

--- zip.js
+++ zip.js
@@ -1,5 +1,6 @@
 var file_system = require("fs");
 var archiver = require("archiver");
+const path = require("path");
 
 var output = file_system.createWriteStream("public/download.zip");
 var archive = archiver("zip");
@@ -17,7 +18,11 @@ archive.on("error", function (err) {
 
 archive.pipe(output);
 
+const input = path.resolve(__dirname, "src/images");
+
+console.log(input);
+
 // append files from a sub-directory, putting its contents at the root of archive
-archive.directory("src/images", false);
+archive.directory(input, false);
 
 archive.finalize();
